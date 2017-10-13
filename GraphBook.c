2f2dd29680273521af3e4ff9fbaa78f2abf865f6
Charles <lolorde@gmail.com>
2017-10-13 11:07:59 -0300

    Create GraphBook.c

---
+++ GraphBook.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+  FILE* file = fopen("Entrada.in","r");
+  char* line;
+  size_t bufSize = 3;
+  
+  
+  
+  double matrix;
+}
